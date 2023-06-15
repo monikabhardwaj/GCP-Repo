7da496e0b786cbfa552216929741ae49205467d4
Monika B <monika.bhardwaj@bjsscloud.net>
2023-06-15 18:12:58 +1000

    Test git CI

--- project/modules/main.tf
+++ project/modules/main.tf
@@ -1,5 +1,5 @@
 resource "google_project" "my_project" {
   name            = "gcp-ctools"
   project_id      = "gcp-ctools"
- # billing_account = "my-billing-account-id"
+  billing_account = "my-billing-account-id"
 }
